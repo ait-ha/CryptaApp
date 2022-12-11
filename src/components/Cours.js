1af9e6d00357168ccbe14fb0a74e23edd024ac42
ait-ha <moah.hammou@yahoo.Fr>
2022-12-11 23:50:45 +0100

    cours

--- src/components/Cours.js
+++ src/components/Cours.js
@@ -11,11 +11,10 @@ import flecheDroite from '../fleche-droite@2x.png';
 
 export default function Cours() {
 
-    let urls = [{"url": '1_video.mp4', "descr": "Vidéo 1/5 : Présentation générale de Crypta-", "poster":'./media/1_video.png'},
-    {"url": "https://youtu.be/jhtJ1zwAkQE", "descr": "Vidéo 2/5 : 2ème Video"},
-    {"url": "https://youtu.be/0ETcLj5jBy4", "descr": "Vidéo 3/5 : 3ème Video"},
-    {"url": "https://www.youtube.com/embed/phgdYixi6cI", "descr": "Vidéo 4/5 : 4ème Video"},
-    {"url": "https://www.youtube.com/embed/phgdYixi6cI", "descr": "Vidéo 5/5 : 4ème Video"}];
+    const [index, setIndex] = useState(0);
+
+    let urls = [{"url": '1_video.mp4', "descr": "Vidéo 1/2 : Présentation générale de Crypta-", "poster":'./media/1_video.png'},
+    {"url": "https://www.youtube.com/watch?v=jAa_qGb83Rg", "descr": "Vidéo 2/2 : 2ème Video"}];
   
     function incrementIndex () {
       if (index < urls.length - 1) {
@@ -28,78 +27,6 @@ export default function Cours() {
       setIndex(index - 1);
     }
    };
-  
-  const [index, setIndex] = useState(0);
-
-const sampleData = [
-    {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#3",
-        "description": "Alchemy's Third NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-];
-const [data, updateData] = useState(sampleData);
-const [dataFetched, updateFetched] = useState(false);
-
-async function getAllNFTs() {
-    const ethers = require("ethers");
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
-    //create an NFT Token
-    let transaction = await contract.getAllNFTs()
-
-  
-    //Fetch all the details of every NFT from the contract and display
-    const items = await Promise.all(transaction.map(async i => {
-        const tokenURI = await contract.tokenURI(i.tokenId);
-        let meta = await axios.get(tokenURI);
-        meta = meta.data;
-
-        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-        let item = {
-            price,
-            tokenId: i.tokenId.toNumber(),
-            seller: i.seller,
-            owner: i.owner,
-            image: meta.image,
-            name: meta.name,
-            description: meta.description,
-        }
-     
-        return item;
-    }))
-
-    updateFetched(true);
-    updateData(items);
-}
-
-if(!dataFetched)
-    getAllNFTs();
 
 return (
     <div>
@@ -109,7 +36,7 @@ return (
         <div>
         
         <video controls
-                    src='1_video.mp4' width="820" />
+                    src={urls[index].url} width="820" />
 
         <p className="descriptif">{urls[index].descr}</p>
         </div>
